9865e071ebc48face87cc8d8a5bfb2e079a8c0b3
Long Nguyen <longnguyen2001@gmail.com>
2022-11-03 11:15:34 -0700

    Add signature signing and finish messaging app profile

--- dataclass.py
+++ dataclass.py
@@ -1,11 +1,61 @@
 import pandas as pd
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+
+
+class MessagingAppProfile:
+
+    def __init__(self):
+        self.id = ""
+        self.private_signatures = {}
+        # Add passphrases to private key for extra security if have time. Not required for demo.
+        # self.passphrases = {saved exported private key, passphrase}
+
+    def load_profile_from_id(self, id: str):
+        df = pd.read_pickle("./data/messagingProfile_account_df.pkl") # need to pickle to use bytes
+        df = df[df['id'] == id]
+
+        self.id = id
+        self.private_signatures = dict(zip(df['Org_name'], df['Private_key']))
+
+    def save_profile(self):
+        df = pd.read_pickle("./data/messagingProfile_account_df.pkl")
+        df = df[df['id'] != self.id]
+
+        updated_profile = pd.DataFrame({
+            "id": [self.id for _ in range(len(self.private_signatures))],
+            "Org_name": list(self.private_signatures.keys()),
+            "Private_key": list(self.private_signatures.values())
+        })
+
+        df = pd.concat([df, updated_profile], ignore_index=True)
+        df.to_pickle("./data/messagingProfile_account_df.pkl")
+
+
+    def add_signature_key(self, org_name: str, key: bytes):
+        self.private_signatures[org_name] = key
+
+    def sign_nonce(self, org_name: str, nonce: bytes):
+        assert org_name in self.private_signatures, f"No key issued from org {org_name}"
+
+        private_key = RSA.import_key(self.private_signatures[org_name])
+        hashed_nonce = SHA256.new(nonce)
+        signed_nonce = pkcs1_15.new(private_key).sign(hashed_nonce)
+
+        return signed_nonce
+
+    def revoke_key(self, org_name): # Probably won't need for MVP, do it if we have time.
+        pass
+
+
 class MasterAccount:
 
     def __init__(self):
         self.org_name = ""
         self.id_key_dict = {}
 
-    def load_account_from_org_name(self, org_name):
+    def load_account_from_org_name(self, org_name: str):
         df = pd.read_pickle("./data/master_account_df.pkl") # need to pickle to use bytes
         df = df[df['Org_name'] == org_name]
         
@@ -27,36 +77,18 @@ class MasterAccount:
         df.to_pickle("./data/master_account_df.pkl")
 
 
-    def issue_private_key(self, new_account):
-        # check that it's actually a new account
-        # add new_account_id : public key to id_key_dict
-        # add the private key to the new_accounts list of signatures
-        pass
+    def issue_private_key(self, new_account: MessagingAppProfile):
+        key = RSA.generate(2048)
+
+        public_key = public_key = key.publickey().export_key()
+        self.id_key_dict[new_account.id] = public_key
+        
+        private_key = key.export_key()
+        new_account.add_signature_key(self.org_name, private_key)
 
     def get_published_public_keys(self):
-        return self.id_key_dict.value()
+        return self.id_key_dict.values()
 
     def revoke_key(self): # Probably won't need for MVP, do it if we have time.
-        pass
-
-class MessagingAppProfile:
-
-    def __init__(self):
-        self.id = ""
-        self.private_signatures = {}
-
-    def load_profile_from_file(self, file_path):
-        pass
-
-    def save_profile_to_file(self, file_path):
-        pass
-
-    def add_signature_key(self, org_name, key):
-        pass
-
-    def sign_nonce(self, nonce):
-        pass
-
-    def revoke_key(self, org_name): # Probably won't need for MVP, do it if we have time.
         pass
 
--- make_mock_data.py
+++ make_mock_data.py
@@ -1,5 +1,9 @@
 from dataclass import MasterAccount, MessagingAppProfile
 import pandas as pd
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 
 def main():
     master_account_df = {
@@ -10,8 +14,17 @@ def main():
     master_account_df = pd.DataFrame(master_account_df)
     master_account_df.to_pickle("./data/mock_master_account_df.pkl")
 
+    messagingAppProfile_df = {
+        "id": ["12345", "12345", "12345"],
+        "Org_name": ["MockOrg1", "MockOrg2", "MockOrg3"],
+        "Private_key": [b'privatekey1', b'privatekey2', b'privatekey3']
+    }
+    messagingAppProfile_df = pd.DataFrame(messagingAppProfile_df)
+    messagingAppProfile_df.to_pickle("./data/mock_messagingProfile_account_df.pkl")
+
 if __name__ == '__main__':
     main()
+    
     test_master_account = MasterAccount()
     test_master_account.load_account_from_org_name("MockOrg")
     test_master_account.save_account()
@@ -19,3 +32,38 @@ if __name__ == '__main__':
 
     print(test_master_account.org_name)
     print(test_master_account.id_key_dict)
+
+    test_messaging_profile = MessagingAppProfile()
+    test_messaging_profile.load_profile_from_id("12345")
+    test_messaging_profile.save_profile()
+    test_messaging_profile.load_profile_from_id("12345")
+    print(test_messaging_profile.id)
+    print(test_messaging_profile.private_signatures)
+
+    key = RSA.generate(2048)
+    
+    private_key = key.export_key()
+    private_key = RSA.import_key(private_key)
+    print(f"private key: {private_key}")
+    print(type(private_key))
+
+    public_key =  key.publickey().export_key()
+    public_key = RSA.import_key(public_key)
+    # print(f"public key: {public_key}")
+
+    data = "testing testing 1 2 3".encode()
+    hashed_data = SHA256.new(data)
+    signed_data = pkcs1_15.new(private_key).sign(hashed_data)
+
+    try:
+        pkcs1_15.new(public_key).verify(hashed_data, signed_data)
+        print("Verification successful")
+
+    except (ValueError, TypeError):
+        print("Verification failed")
+
+    # cipher_rsa = PKCS1_OAEP.new(public_key)
+    # encrypted_data = cipher_rsa.encrypt(data)
+
+    # cipher_rsa = PKCS1_OAEP.new(private_key)
+    # decrypted_data = cipher_rsa.decrypt(encrypted_data)
